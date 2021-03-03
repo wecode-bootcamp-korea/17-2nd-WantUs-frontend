a386375083caee402f47e4d547060875b5b704ca
peng0code <peng0code>
2021-03-03 10:10:05 +0900

    ADD: 리뷰 반영 완료 (함수 수정 및 styled component 수정 완료)

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from './Components/Nav/Nav'
+import Nav from './Components/Nav/Nav';
+import Main from './Pages/Main';
 
 function Routes() {
   return (
     <Router>
       <Nav />
       <Switch>
-      <Route exact path="/" component={Nav} />
-    </Switch>
-  </Router>
-  )
+        <Route exact path="/" component={Main} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default Routes
+export default Routes;

782987d25b43c3ad9af9c94b50644b126eaedf98
peng0code <peng0code>
2021-03-07 14:02:59 +0900

    ADD: 리뷰 사항 반영 완료(git rebase)

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const introPic1 =
+  'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80';
+
+export const introPic5 =
+  'https://images.unsplash.com/photo-1604156787443-f27bfdee09fc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80';

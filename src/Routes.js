4ac6d226833b6001c1347308982f2919b8f1450c
peng0code <peng0code>
2021-03-09 19:32:08 +0900

    ADD: 컴플릭트 해결 완료

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import Footer from './Components/Footer/Footer';
 import Newintro from './Pages/Newintro/Newintro';
+import Main from './Pages/Main/Main';
 
 function Routes() {
   return (
@@ -10,6 +11,7 @@ function Routes() {
       <Nav />
       <Switch>
         <Route exact path="/newintro" component={Newintro} />
+        <Route exact path="/" component={Main} />
       </Switch>
       <Footer />
     </Router>
